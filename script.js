44025f4ac3689fc6970399c395e2c6c9ee166adb
George Kolev <gogo1520180@gmail.com>
2020-05-01 20:15:15 +0200

    1

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var sound = document.getElementById('sound');
+var play = document.getElementById('spagettiCode');
+
+play.onclick = function () {
+console.log('play');
+sound.play();
+return false;
+};
+//mmmmvanillajavascript:^)
